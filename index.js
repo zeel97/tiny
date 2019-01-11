a5e80eacb97bb26e29bc75048f8c89f12adad5af
Zeel Mehta <zeelmehta@Zeels-MacBook.local>
2019-01-11 09:35:14 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-module.exports = function() {
-
-  function tiny(string) {
+module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
   };
 
-  function print(string) {
-    return console.log(string);
-  }
-
-}
   
