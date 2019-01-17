dcf921606372294c19eff008609c20ce9f0ffae0
Zeel Mehta <zeelmehta@Zeels-MacBook.local>
2019-01-17 17:22:41 +0530

    multiple functions

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+const logger = (string) => {
+  return string
+}
+
+const lowerCase = (string) => {
+  return lowerCase(string)
+}
+
+const tiny = (string) => {
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
-  };
+}
+
+module.exports = {
+  logger,
+  lowerCase,
+  tiny
+}
 
   
